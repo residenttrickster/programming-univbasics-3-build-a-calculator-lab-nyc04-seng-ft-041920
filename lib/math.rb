0209defadd41747011e2403cfa57df45766100c8
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-23 01:44:50 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+sum
 end
 
 def subtraction(num1, num2)
-
+subtract = num1 - num2
+subtract
 end
 
 def division(num1, num2)
-
+divide = num1 / num2
+divide
 end
 
 def multiplication(num1, num2)
-
+multi = num1 * num2
+multi
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+mod 
 end
 
 def square_root(num)
-
+square = Math.sqrt(num)
+square
 end
